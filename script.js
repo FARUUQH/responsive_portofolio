3c57dddddc17410c56d76c4c85b7c7d8098689a9
FARUUQH <inametion9@gmail.com>
2025-09-18 09:39:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Global variable 
+const themes = ['default', 'blue', 'green'];
+let currentTheme = 0;
+
+// DOM Elements
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const themeBtn = document.getElementById('theme-btn');
+const themeIcon = document. querySelector('.theme-icon');
+
+// Mobile Nav toggle 
+hamburger.addEventListener('click', function () {
+    navMenu.classList.toggle('active');
+});
+
+// Close mobile menu when clicking on a link //
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function () {
+        navMenu.classList.remove('active');
+    });
+})
+// Theme Switcher
+
+themeBtn.addEventListener('click', function () {
+    currentTheme = (currentTheme + 1) % themes.length;
+    document.body.setAttribute('data-theme', themes[currentTheme])
+
+    //    update icon 
+    const icons = ['🌈', '🫐', '🤑'];
+    themeIcon.textContent = icons[currentTheme];
+
+});
